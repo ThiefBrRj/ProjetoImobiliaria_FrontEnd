1dd794fd12bb3f14071f4d9631c1f595dca2b4d3
Thiago <thiagofagundesdossantos@gmail.com>
2022-10-24 10:43:43 -0300

    test

---
+++ js/414.ac6b1626.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvuetify_imobiliaria"]=self["webpackChunkvuetify_imobiliaria"]||[]).push([[414],{2414:function(n,t,e){e.r(t),e.d(t,{default:function(){return s}});var i=e(720),a=function(){var n=this,t=n._self._c;return t(i.Z,[t("v-cotainer",[t("div",{staticClass:"block text-center"},[t("h2",[n._v("Página não encontrada!")])])])],1)},u=[],r={name:"NotFound",data(){return{itens:[]}}},l=r,c=e(1001),o=(0,c.Z)(l,a,u,!1,null,null,null),s=o.exports}}]);
+//# sourceMappingURL=414.ac6b1626.js.map
